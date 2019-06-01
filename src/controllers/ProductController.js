e5bb90ce4cd3fc493fd94d6479b63e5b6509d623
Guilherme Pejon <guilpejon@gmail.com>
2019-06-01 11:24:45 -0300

    Finished products CRUD

--- src/controllers/ProductController.js
+++ src/controllers/ProductController.js
@@ -9,9 +9,30 @@ module.exports = {
     return res.json(products);
   },
 
+  async show(req, res) {
+    const product = await Product.findById(req.params.id);
+
+    return res.json(product);
+  },
+
   async store(req, res) {
     const product = await Product.create(req.body);
 
     return res.json(product);
+  },
+
+  async update(req, res) {
+    // new: true is required to return the updated object, not the old one
+    const product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+
+    return res.json(product);
+  },
+
+  async destroy(req, res) {
+    await Product.findByIdAndRemove(req.params.id);
+
+    return res.send();
   },
 };
--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,9 @@ const routes = express.Router();
 const ProductController = require('./controllers/ProductController.js');
 
 routes.get('/products', ProductController.index);
+routes.get('/product/:id', ProductController.show);
 routes.post('/products', ProductController.store);
+routes.put('/products/:id', ProductController.update);
+routes.delete('/products/:id', ProductController.destroy);
 
 module.exports = routes;
